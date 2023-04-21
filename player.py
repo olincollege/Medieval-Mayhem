fa495c08173826c3f1d722c9586703a1ea36f34c
Prisha Bhatia <pbhatia@olin.edu>
2023-04-21 13:24:41 -0400

    Fix simple syntax issue

--- player.py
+++ player.py
@@ -25,15 +25,9 @@ class Player(pygame.sprite.Sprite):
             # Lines 26-28 only works if person running the code has the
             # files stored on their computer
             picture = pygame.image.load(
-                os.path.join("images", "hero" + str(i) + "png")
+                os.path.join("images", "hero" + str(i) + ".png")
             ).convert()
             self.images.append(picture)
             self.image = self.images[0]
             self.rect = self.image.get_rect()
-
-        
-
-
-
-
 
