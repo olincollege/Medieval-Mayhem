b2ec0cfe01c7a8bb3e468f208ca53f1fb8da71f1
MihirV17 <mvemuri@olin.edu>
2023-04-21 14:34:54 -0400

    Commiting everything so images folder works

---
+++ main.py
@@ -0,0 +1,55 @@
+import pygame
+import sys
+import os
+from player import Player
+
+
+worldx = 960
+worldy = 720
+fps = 40  # frame rate
+ani = 4  # animation cycles
+world = pygame.display.set_mode([worldx, worldy])
+
+BLUE = (25, 25, 200)
+BLACK = (23, 23, 23)
+WHITE = (254, 254, 254)
+ALPHA = (0, 255, 0)
+
+
+backdrop = pygame.image.load(os.path.join("background.PNG"))
+clock = pygame.time.Clock()
+pygame.init()
+backdropbox = world.get_rect()
+main = True
+
+player = Player()  # spawn player
+player.rect.x = 0  # go to x
+player.rect.y = 0  # go to y
+player_list = pygame.sprite.Group()
+player_list.add(player)
+
+
+"""
+Main Loop
+"""
+
+while main:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            try:
+                sys.exit()
+            finally:
+                main = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == ord("q"):
+                pygame.quit()
+            try:
+                sys.exit()
+            finally:
+                main = False
+    world.blit(backdrop, backdropbox)
+    player_list.draw(world)
+    pygame.display.flip()
+    clock.tick(fps)
--- player.py
+++ player.py
@@ -11,23 +11,12 @@ class Player(pygame.sprite.Sprite):
     """
 
     def __init__(self):
-        """
-        Method to create the object (penguin) desired on screen
-
-        Args:
-            self: an instance of the class
-        """
         pygame.sprite.Sprite.__init__(self)
         self.images = []
-
-        # Loop to cycle through each image
-        for i in range(1, 5):
-            # Lines 26-28 only works if person running the code has the
-            # files stored on their computer
-            picture = pygame.image.load(
-                os.path.join("images", "hero" + str(i) + ".png")
+        for i in range(1, 3):
+            img = pygame.image.load(
+                os.path.join("images", "DragonFly" + str(i) + ".png")
             ).convert()
-            self.images.append(picture)
+            self.images.append(img)
             self.image = self.images[0]
             self.rect = self.image.get_rect()
-
