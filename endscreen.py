252d99c0f76bd2aee5922979ee2e76d15567c23b
Prisha Bhatia <pbhatia@olin.edu>
2023-05-01 13:30:27 -0400

    Initial edits to increase pylint score

--- endscreen.py
+++ endscreen.py
@@ -1,3 +1,7 @@
+"""
+File containing EndScreen class
+"""
+
 import pygame
 from background import Background as Bg
 
@@ -30,7 +34,9 @@ class EndScreen:
         # Create font objects for end screen text and button text
         self.end_font = pygame.font.Font(None, 60)
         self.end_text = self.end_font.render("Game Over!", True, (255, 0, 0))
-        self.end_rect = self.end_text.get_rect(center=(self.width / 2, self.height / 2))
+        self.end_rect = self.end_text.get_rect(
+            center=(self.width / 2, self.height / 2)
+            )
         self.button_font = pygame.font.Font(None, 30)
         self.button_text = self.button_font.render("Exit", True, (0, 0, 0))
         self.button_rect = self.button_text.get_rect(
@@ -42,7 +48,8 @@ class EndScreen:
 
     def display(self):
         """
-        Display the end screen until the user clicks the Exit button or closes the window.
+        Display the end screen until the user
+        clicks the Exit button or closes the window.
 
         Returns:
             bool: True if the Exit button is clicked, False otherwise
@@ -50,17 +57,21 @@ class EndScreen:
         while not self.done:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    # If the user clicks the 'x' button in the top right corner of the window, set done flag to True
+                    # If the user clicks the 'x' button in the top right corner
+                    # of the window, set done flag to True
                     self.done = True
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    # If the user clicks the mouse button, get the position of the click
+                    # If the user clicks the mouse button
+                    # get the position of the click
                     x, y = event.pos
-                    # If the position of the click is within the bounds of the Exit button, set done flag to True
+                    # If the position of the click is within the bounds of the
+                    # Exit button, set done flag to True
                     if self.button_rect.collidepoint(x, y):
                         self.done = True
                         return True
 
-            # Blit the end screen text, button background, and button text onto the game window
+            # Blit the end screen text, button background,
+            # and button text onto the game window
             Bg.world.blit(self.end_text, self.end_rect)
             pygame.draw.rect(Bg.world, (255, 255, 255), self.button_rect)
             Bg.world.blit(self.button_text, self.button_rect)
