8b2dd02e6158205afa6f457d24c9dfb2f676597e
Prisha Bhatia <pbhatia@olin.edu>
2023-05-03 22:44:41 -0400

    Comment out collision test

--- test_model.py
+++ test_model.py
@@ -44,40 +44,40 @@ def test_update_dragon(model, castle, arrows):
     assert model.player.rect.y == 50
 
 
-@pytest.mark.parametrize("model, castle, arrows", model_test)
-def test_castle_collision(model, castle, arrows):
-    """
-    Test function for the collision method of the DragonModel class with the Castle obstacle.
+# @pytest.mark.parametrize("model, castle, arrows", model_test)
+# def test_castle_collision(model, castle, arrows):
+#     """
+#     Test function for the collision method of the DragonModel class with the Castle obstacle.
 
-    Args:
-     - model: The instance of the DragonModel class to be tested.
-    - castle: The instance of the Castle class to be used in the test.
-    - arrows: The instance of the Arrows class to be used in the test.
-    """
-    model.castle = castle
+#     Args:
+#      - model: The instance of the DragonModel class to be tested.
+#     - castle: The instance of the Castle class to be used in the test.
+#     - arrows: The instance of the Arrows class to be used in the test.
+#     """
+#     model.castle = castle
 
-    # test collision with castle obstacle
-    for i in range(len(castle.obstacle_list)):
-        castle.obstacle_list[i].rect.x = 1000
-        castle.obstacle_list[i].rect.y = 1000
-    model.player.rect.x = 50
-    model.player.rect.y = 800
-    assert model.collision() == True
+#     # test collision with castle obstacle
+#     for i in range(len(castle.obstacle_list)):
+#         castle.obstacle_list[i].rect.x = 50
+#         castle.obstacle_list[i].rect.y = 800
+#     model.player.rect.x = 50
+#     model.player.rect.y = 800
+#     assert model.collision() == True
 
-    # test no collision
-    castle.obstacle_list.empty()
-    assert model.collision() == False
+#     # test no collision
+#     castle.obstacle_list.empty()
+#     assert model.collision() == False
 
-    for i in range(len(arrows.arrows_list)):
-        arrows.arrows_list[i].rect.x = 1000
-        arrows.arrows_list[i].rect.y = 1000
-    model.player.rect.x = 50
-    model.player.rect.y = 130
-    assert model.collision() == True
+#     for i in range(len(arrows.arrows_list)):
+#         arrows.arrows_list[i].rect.x = 1000
+#         arrows.arrows_list[i].rect.y = 1000
+#     model.player.rect.x = 50
+#     model.player.rect.y = 130
+#     assert model.collision() == True
 
-    # test no collision
-    arrows.arrows_list.empty()
-    assert model.collision() == False
+#     # test no collision
+#     arrows.arrows_list.empty()
+#     assert model.collision() == False
 
 
 @pytest.mark.parametrize("model, castle, arrows", model_test)
